0f0cfa91b73c84245f984020090ceb99eafd6d1d
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-07 16:29:26 +0600

    add new looping work practice

---
+++ hometask.js
@@ -0,0 +1,29 @@
+// const fruits = ['mango', 'jackfruit', 'lichi', 'watermelon'];
+
+// console.log(fruits[3]);
+
+// fruits[2] = 'jambura';
+
+// console.log(fruits);
+
+//--------------------------------------------------
+
+let city = 'dhaka';
+let friendsName = ['harun', 'jaman','saykat'];
+let age = 25;
+let scoreV = [25, 55, 66];
+let isActive = true;
+
+console.log(Array.isArray(isActive));
+
+const numbers = [25,64,78,752,965,785,48];
+
+for (const num of numbers){
+ console.log(num);   
+}
+
+const fruits = ['orange','mango','watermelon','jam']
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to fruit');
+}
---
+++ while.js
@@ -0,0 +1,15 @@
+const number = 7;
+if (number < 10) {
+    console.log('cotto akta number paici');
+}
+console.log('somthing new is cooking');
+
+
+let num = 0;
+
+while (num < 5) {
+    console.log('looooooooooing');
+    num = num + 1;
+    // num++;
+    // num += 1;
+}
